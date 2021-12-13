85c6f995734ce6300f97b9539664b9dfb0e47115
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2021-12-13 23:17:41 +0900

    Refinder

--- background.js
+++ background.js
@@ -103,7 +103,7 @@ chrome.downloads.onChanged.addListener(async ({ id, ...downloadItemDelta }) => {
           }
 
           formData.append("access_token", gyazoAccessToken);
-          formData.append("app", "Download Salvager");
+          formData.append("app", "Download Refinder");
           formData.append("imagedata", canvasBlob);
           formData.append("referer_url", downloadItem.url);
 
